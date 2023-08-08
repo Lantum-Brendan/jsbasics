88bf2d9a8497b3f84ae1178105ce8279ee30c645
Lantum Brendan <lantumbrendan170@gmail.com>
2023-08-08 15:34:28 +0100

    Javasscript quiz app

---
+++ questions.js
@@ -0,0 +1,112 @@
+//creating an array and passing the number, questions, options and answers
+let questions = [
+   {
+      numb: 1,
+      question: "Who is the VC of UB?",
+      answer: "Prof. Ngomo Horrace",
+      options:[
+         "Prof. Nalova Lyonga",
+         "Prof. Ngomo Horrace",
+         "Prof. Eneke Bechem",
+         "Prof. Jacques Fame "
+      ]
+   },
+   {   numb: 2,
+      question: "What does CSS stand for?",
+      answer: "Cascading Style Sheet",
+      options:[
+         "Common Style Sheet",
+         "Colorful Style Sheet",
+         "Computer Style Sheet",
+         "Cascading Style Sheet"
+      ]
+   },
+   {
+      numb: 3,
+      question: "What does PHP stand for?",
+      answer: "Hyper Text Preprocessor",
+      options:[
+         "Hyper Text Preprocessor",
+         "Hyper TExt programming",
+         "Hyper Text preprogramming",
+         "Hometext Preprocessor"
+      ]
+   },
+   {
+      numb: 4,
+      question: "What is the full form of CPU?",
+      answer: "Central Processing Unit",
+      options:[
+         "Computer Processing Unit",
+         "Computer Principle Unit",
+         "Central Processing Unit",
+         "Control Processing Unit"
+      ]
+   },
+   {
+      numb: 5,
+      question: "Which of the following is the brain of the computer?",
+      answer: "Central Processing Unit",
+      options:[
+         "Central Processing Unit",
+         "Memory",
+         "Arithmetic and Logic unit",
+         "Control unit"
+      ]
+   },
+   {
+      numb: 6,
+      question: "Which of the following is the smallest unit of data in a computer?",
+      answer: "Bit",
+      options:[
+         "Bit",
+         "KB",
+         "Nibble",
+         "Byte"
+      ]
+   },
+   {
+      numb: 7,
+      question: "Which of the following are physical devices of a computer?",
+      answer: "Hardware",
+      options:[
+         "Hardware",
+         "Software",
+         "System Software",
+         "Package"
+      ]
+   },
+   {
+      numb: 8,
+      question: "OSS stands for:",
+      answer: "Open Source Software",
+      options:[
+         "Open System Service",
+         "Open Source Software",
+         "Open System Software",
+         "Open Synchronized Software"
+      ]
+   },
+   {
+      numb: 9,
+      question: "Public domain software is usually:",
+      answer: "Community supported",
+      options:[
+         "System supported",
+         "Source supported",
+         "Community supported",
+         "Programmer supported"
+      ]
+   },
+   {
+      numb: 10,
+      question: "Assembler is used as a translator for?",
+      answer: " Low level language",
+      options:[
+         " Low level language",
+         "High Level Language",
+         "COBOL",
+         "C"
+      ]
+   }
+];
